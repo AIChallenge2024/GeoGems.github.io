7ae7f013605423cb768657dbc27e8a5e7b62bbcd
Rime6 <rimenasser6@gmail.com>
2024-11-28 20:16:15 -0500

    Added the interactive map with markers, popup messages and a side bar that pops up when you click on a point in the map with the coordinates

---
+++ map.js
@@ -0,0 +1,58 @@
+function initMap(){
+    //Map Options
+    var options = {
+        zoom:13,
+        center:{lat:37.98450557467865, lng:23.7275026629592}
+    }
+    //New Map
+    var map = new 
+    google.maps.Map(document.getElementById("map"), options);
+
+    //Add Marker Function
+    function addMarker(props){
+        var marker = new google.maps.Marker({
+            position:props.coordinates,
+            map:map,
+        });
+
+        //Check if icon and content were declared in function
+        if (props.iconImage){
+            marker.setIcon(props.iconImage)
+        }
+        if (props.content){
+            var infoWindow = new google.maps.InfoWindow({
+                content:props.content
+            })
+        
+            marker.addListener("click", function(){
+                infoWindow.open(map, marker);
+            })
+        }
+    }
+    const gem_icon = {
+        url: "https://www.clker.com/cliparts/5/8/7/3/1566560070407030839map-marker-purple.png", // URL to the image
+        scaledSize: new google.maps.Size(28, 40)
+      };
+
+    const touristic_icon = {
+        url: "https://www.pacificcarrentals.com/wp-content/uploads/2020/02/Location.png", // URL to the image
+        scaledSize: new google.maps.Size(28, 40)
+      };
+    addMarker({coordinates:{lat:37.97251240995257, lng:23.72807410862482},iconImage:gem_icon, content:"<h3>Anafiotika</h3>"});
+    addMarker({coordinates:{lat:37.977138803582775, lng:23.72290740873045},iconImage:gem_icon, content:"<h3>Ta Karamanlidika tou Fani</h3>"});
+    addMarker({coordinates:{lat:37.97149029175632, lng:23.725724793151805}, iconImage:touristic_icon, content:"<h3>Acropolis</h3>"});
+    
+    //Listen for click on map
+    map.addListener("click", (event) => {
+        const lat = event.latLng.lat();
+        const lng = event.latLng.lng();
+        const infobox = document.getElementById("info");
+        infobox.style.display = "block"
+        infobox.innerHTML = `
+          <h2>Location Details</h2>
+          <p>Latitude: ${lat}</p>
+          <p>Longitude: ${lng}</p>
+          <p>Additional information can go here.</p>
+        `;
+      });
+}
