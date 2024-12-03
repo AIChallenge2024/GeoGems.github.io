ed019eabe321548f084a62dd651b556012f340ec
Rayen-BT <rayenbentamarzizt@gmail.com>
2024-12-03 01:55:01 -0500

    Added a search bar and tried to make the filter thing work but like wtf :(

---
+++ hidden_gems.js
@@ -0,0 +1,98 @@
+let locations = {
+    data: [
+      {
+        locationName: "Anafiotika",
+        category: "Nature park",
+        image: "Pictures/anafiotika.png",
+      },
+      {
+        locationName: "Colosseum",
+        category: "Sights & Landmarks",
+        image: "Pictures/colosseum.png",
+      },
+    ],
+  };
+  
+  for (let i of locations.data) {
+    //Create Card
+    let card = document.createElement("div");
+    //Card should have category and should stay hidden initially
+    card.classList.add("card", i.category, "hide");
+    //image div
+    let imgContainer = document.createElement("div");
+    imgContainer.classList.add("image-container");
+    //img tag
+    let image = document.createElement("img");
+    image.setAttribute("src", i.image);
+    imgContainer.appendChild(image);
+    card.appendChild(imgContainer);
+    //container
+    let container = document.createElement("div");
+    container.classList.add("container");
+    //location name
+    let name = document.createElement("h5");
+    name.classList.add("location-name");
+    name.innerText = i.locationName.toUpperCase();
+    container.appendChild(name);
+  
+    card.appendChild(container);
+    document.getElementById("locations").appendChild(card);
+  }
+  
+  //parameter passed from button (Parameter same as category)
+  function filterLocation(value) {
+    //Button class code
+    let buttons = document.querySelectorAll(".button-value");
+    buttons.forEach((button) => {
+      //check if value equals innerText
+      if (value.toUpperCase() == button.innerText.toUpperCase()) {
+        button.classList.add("active");
+      } else {
+        button.classList.remove("active");
+      }
+    });
+  
+    //select all cards
+    let elements = document.querySelectorAll(".card");
+    //loop through all cards
+    elements.forEach((element) => {
+      //display all cards on 'all' button click
+      if (value == "all") {
+        element.classList.remove("hide");
+      } else {
+        //Check if element contains category class
+        if (element.classList.contains(value)) {
+          //display element based on category
+          element.classList.remove("hide");
+        } else {
+          //hide other elements
+          element.classList.add("hide");
+        }
+      }
+    });
+  }
+  
+  //Search button click
+  document.getElementById("search").addEventListener("click", () => {
+    //initializations
+    let searchInput = document.getElementById("search-input").value;
+    let elements = document.querySelectorAll(".location-name");
+    let cards = document.querySelectorAll(".card");
+  
+    //loop through all elements
+    elements.forEach((element, index) => {
+      //check if text includes the search value
+      if (element.innerText.includes(searchInput.toUpperCase())) {
+        //display matching card
+        cards[index].classList.remove("hide");
+      } else {
+        //hide others
+        cards[index].classList.add("hide");
+      }
+    });
+  });
+  
+  //Initially display all locations
+  window.onload = () => {
+    filterLocation("all");
+  };
